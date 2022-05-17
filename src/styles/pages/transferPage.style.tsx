0111ee48aebcf57d4111079e914f5e1b561977b4
Guga <contato.gustavobarbosa@outlook.com>
2022-05-17 15:16:14 -0300

    Preparando revisão de código

--- src/styles/pages/transferPage.style.tsx
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-import { HomeContainer } from "./home.style";
-
-const TransferContainer = styled(HomeContainer)`
-    padding-top: 90px;
-    justify-content: flex-start;
-`;
-
-export { TransferContainer };
